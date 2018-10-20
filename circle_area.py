d522ed617e9ce8250f2c09e544eb6dcb947da3c1
Panagonova <43958507+Panagonova@users.noreply.github.com>
2018-10-20 21:35:21 +0300

    calculate circle area

---
+++ circle_area.py
@@ -0,0 +1,8 @@
+import math
+
+radius = float(input("Enter radius: "))
+circle_area = math.pi * radius * radius
+perimeter = 2 * math.pi * radius
+
+print("The perimeter is: %.2f" % perimeter)
+print("The area is: %.2f" % circle_area)
