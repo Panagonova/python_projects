9d66e53d1065131d3f16bf290d454610fb0522c0
Panagonova <43958507+Panagonova@users.noreply.github.com>
2018-12-01 12:18:27 +0200

    for loop

---
+++ For-Loop/int_sum.py
@@ -0,0 +1,7 @@
+n = int(input())
+number_sum = 0
+
+for i in range(1, n + 1):
+   current_num = int(input())
+   number_sum = number_sum + current_num
+print(number_sum)
---
+++ For-Loop/latin_letters.py
@@ -0,0 +1,3 @@
+for i in range(ord('a'), ord('z') + 1):
+    print(chr(i))
+
